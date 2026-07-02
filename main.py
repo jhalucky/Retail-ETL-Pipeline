34d2fc93554aeb367f8ddc1f0e3531bad1017323
Lucky Jha <jhalucky61@gmail.com>
2026-07-02 21:26:50 +0530

    Validation validate_missing_val, validate_duplicate_rows

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.extract.read_csv import read_csv
-from src.validate.validator import validate_missing_values
+from src.validate.validator import validate_missing_values, validate_duplicate_rows
 customers_df =  read_csv('data/raw/customers.csv')
 
 validate_missing_values(customers_df)
+validate_duplicate_rows(customers_df)
--- src/validate/validator.py
+++ src/validate/validator.py
@@ -7,3 +7,15 @@ def validate_missing_values(df):
     print(missing_values)
 
     return missing_values
+
+
+
+def validate_duplicate_rows(df):
+    """Validate for duplicate rows in the DataFrame."""
+    
+    duplicate_rows = df.duplicated().sum()
+
+    print("\nDuplicate rows:")
+    print(duplicate_rows)
+
+    return duplicate_rows
