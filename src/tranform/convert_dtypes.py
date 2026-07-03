7d4050e591c65f55ce6767a6c8f703be45dc3dc1
Lucky Jha <jhalucky61@gmail.com>
2026-07-03 09:44:43 +0530

    transform-convert_dtypes

---
+++ src/tranform/convert_dtypes.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def convert_dtypes(df):
+    """
+    Converts the data types of columns in a DataFrame to appropriate types.
+
+    Parameters:
+        df (pd.DataFrame): The input DataFrame."""
+    
+    if "signup_date" in df.columns:
+        df["signup_date"] = pd.to_datetime(df["signup_date"], errors="coerce")
+
+    if "order_date" in df.columns:
+        df["order_date"] = pd.to_datetime(df["order_date"], errors="coerce")
+
+    if "payment_date" in df.columns:
+        df["payment_date"] = pd.to_datetime(df["payment_date"], errors="coerce")
+    
+    return df
